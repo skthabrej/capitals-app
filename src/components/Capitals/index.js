0ef3b7e94d3c48983c395a79b624d984704bd98a
Shaik Thabrej Basha <skthabrej@gmail.com>
2022-10-15 13:27:52 +0530

    capital letters

---
+++ src/components/Capitals/index.js
@@ -0,0 +1,71 @@
+import { Component } from "react";
+import './index.css'
+
+const countryAndCapitalsList = [
+    {
+      id: 'NEW_DELHI',
+      capitalDisplayText: 'New Delhi',
+      country: 'India',
+    },
+    {
+      id: 'LONDON',
+      capitalDisplayText: 'London',
+      country: 'United Kingdom',
+    },
+    {
+      id: 'PARIS',
+      capitalDisplayText: 'Paris',
+      country: 'France',
+    },
+    {
+      id: 'KATHMANDU',
+      capitalDisplayText: 'Kathmandu',
+      country: 'Nepal',
+    },
+    {
+      id: 'HELSINKI',
+      capitalDisplayText: 'Helsinki',
+      country: 'Finland',
+    },
+  ]
+  
+
+class Capitals extends Component {
+    state = {
+        countryId:countryAndCapitalsList[0].id,
+        country:countryAndCapitalsList[0].country
+    }
+
+    selectedCountry = event => {
+        const resultsCapital = countryAndCapitalsList.filter(eachItem =>
+            eachItem.id === event.target.value
+            )
+        this.setState({
+            countryId:event.target.value,
+            country:resultsCapital[0].country
+        })
+    }
+
+    render() {
+        const {countryId,country} = this.state
+        return (
+            <div className="bg-container">
+                <div className="card-container">
+                    <h1 className="heading">Countries and Capitals</h1>
+                    <div className="options-container">
+                        <select className="select" onChange={this.selectedCountry} value={countryId}>
+                            {countryAndCapitalsList.map(eachCountry => (
+                                <option className="option" key={eachCountry.id} value={eachCountry.id}>
+                                    {eachCountry.capitalDisplayText}
+                                </option>
+                            ))}
+                        </select>
+                        <p className="paragraph">is capital of which country</p>
+                    </div>
+                    <p className="country">{country}</p>
+                </div>
+            </div>
+        )
+    }
+}
+export default Capitals
